8e1b0c9f90a0babceac58020fc29ed5752d44d8b
NTWebcreator <ntcreator@hotmail.com>
2023-12-31 14:25:07 +0900

    双方向通信のテストプログラムが完成した。

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var wsServer = new WebSocket("wss://127.0.0.1:8000");//この部分は一般公開でき次第URLを変更。
+var wsServer = new WebSocket("ws://127.0.0.1:8000");//この部分は一般公開でき次第URLを変更。
 document.getElementById("sendbutton").addEventListener("click",
 async (e)=>{
     var sendText = document.getElementById("textarea").value;
     wsServer.send(sendText);
+});
+wsServer.addEventListener("message",
+async (e)=>{
+    console.log(e.data);
 });
