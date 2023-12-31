09945cdcf4f64e26233c8165b3aef9ece9393474
NTWebcreator <ntcreator@hotmail.com>
2023-12-31 10:31:29 +0900

    websocketのテストプログラムが完成。

---
+++ index.js
@@ -0,0 +1,6 @@
+var wsServer = new WebSocket("wss://127.0.0.1:8000");//この部分は一般公開でき次第URLを変更。
+document.getElementById("sendbutton").addEventListener("click",
+async (e)=>{
+    var sendText = document.getElementById("textarea").value;
+    wsServer.send(sendText);
+});
